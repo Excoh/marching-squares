29cc34ab7c5fb37157434a4f0bd5ad4d0c59aab9
Excoh <excel.ortega@gmail.com>
2024-08-27 07:28:17 -0700

    Grid changes

--- grid.gd
+++ grid.gd
@@ -11,10 +11,16 @@ extends Node2D
 		grid_color = value
 		queue_redraw()
 
-@export var resolution: int = 10
-@export var grid_size: Vector2 = Vector2(600, 400):
+@export_range(5, 50) var resolution: int = 10:
+	set(value):
+		resolution = value
+		update_cols_rows()
+		queue_redraw()
+
+@export var grid_size: Vector2i = Vector2i(600, 400):
 	set(value):
 		grid_size = value
+		update_cols_rows()
 		queue_redraw()
 
 var cols: int
@@ -22,8 +28,8 @@ var rows: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	cols = int (grid_size.x / resolution)
-	rows = int (grid_size.y / resolution)
+	cols = int(grid_size.x / resolution)
+	rows = int(grid_size.y / resolution)
 	queue_redraw()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -31,6 +37,13 @@ func _process(delta: float) -> void:
 	pass
 
 func _draw() -> void:
+	draw_grid()
+
+func update_cols_rows() -> void:
+	cols = int(grid_size.x / resolution)
+	rows = int(grid_size.y / resolution)
+
+func draw_grid() -> void:
 	for i in range(cols):
 		for j in range(rows):
 			draw_circle(Vector2(i*resolution, j*resolution), radius, grid_color)
